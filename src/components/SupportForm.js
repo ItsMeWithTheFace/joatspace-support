fac817fd9713dec4fc4d5580f0d22c8941d32dad
rakinuddin <rakinuddin97@gmail.com>
2018-11-25 13:12:54 -0500

    documentation and cleanup in support form

--- src/components/SupportForm.js
+++ src/components/SupportForm.js
@@ -3,8 +3,10 @@ import { Card, Button, Form, FormGroup, Label, Input } from 'reactstrap';
 import * as emailjs from 'emailjs-com';
 import config from '../config';
 
-// this manages the state of the contact form separately 
-// from the support page
+/*
+  this manages the state of the contact form separately 
+  from the support page
+*/
 class SupportForm extends Component {
   constructor(props) {
     super(props)
@@ -15,19 +17,25 @@ class SupportForm extends Component {
       text: '',
       formSubmitted: false
     }
+
+    this.handleChange.bind(this)
+    this.handleSubmit.bind(this)
   }
 
-  handleChange = async (event) => {
+  // set the correct state value
+  handleChange = (event) => {
     const { name, value } = event.target;
 
-    await this.setState({
+    this.setState({
       [ name ]: value
     });
   }
 
+  // sends email using emailjs library
   handleSubmit = (event) => {
     event.preventDefault();
 
+    // some parameters set in src/config.js
     const emailParams = {
       from_name: this.state.name,
       from_email: this.state.email,
@@ -37,46 +45,60 @@ class SupportForm extends Component {
     }
 
     emailjs.init(config.emailjs.REACT_APP_EMAILJS_USERID);
-    console.log("about to send")
+
     emailjs.send(config.emailjs.SERVICE_ID,
       config.emailjs.REACT_APP_EMAILJS_TEMPLATEID,
       emailParams)
-    .then(res => {
-      console.log(res);
+    .then(() => {
       this.setState({ formSubmitted: true })
     })
     .catch(err => {
       console.error(err)
     })
+    .finally(() => {
+      this.resetFields()
+    })
+  }
+
+  // resets the text fields after submission
+  resetFields = () => {
+    this.setState({
+      name: '',
+      email: '',
+      text: ''
+    })
   }
 
   render = () => {
-    const formStyle = {
+    const cardStyle = {
+      margin: '5%'
+    }
+
+    const contentStyle = {
       padding: '3rem',
       textAlign: 'justify'
     }
 
     return (
-      <Card>
-        <Form style={formStyle} onSubmit={this.handleSubmit}>
+      <Card style={cardStyle}>
+        <Form style={contentStyle} onSubmit={this.handleSubmit}>
           <FormGroup>
             <Label for="fullName">Full Name</Label>
-            <Input type="text" required name="name" id="fullName" onChange={this.handleChange} placeholder="Enter your name here"/>
+            <Input type="text" required name="name" id="fullName" value={this.state.name} onChange={this.handleChange} placeholder="Enter your name here"/>
           </FormGroup>
           <FormGroup>
             <Label for="fromEmail">Email</Label>
-            <Input type="email" required name="email" id="fromEmail" onChange={this.handleChange} placeholder="Enter your e-mail here"/>
+            <Input type="email" required name="email" id="fromEmail" value={this.state.email} onChange={this.handleChange} placeholder="Enter your e-mail here"/>
           </FormGroup>
           <FormGroup>
             <Label for="inquiryText">Inquiry</Label>
-            <Input type="textarea" name="text" id="inquiryText" onChange={this.handleChange} placeholder="Enter your issue..."/>
+            <Input type="textarea" name="text" id="inquiryText" value={this.state.text} onChange={this.handleChange} placeholder="Enter your issue..."/>
           </FormGroup>
           <Button type="submit" value="Submit">
             Submit
           </Button>          
         </Form>
       </Card>
-
     )
   }
 }
