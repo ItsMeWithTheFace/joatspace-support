1dee2828091980baf7e0c375e4b214f249dda884
rakinuddin <rakinuddin97@gmail.com>
2018-11-25 13:12:36 -0500

    cleaned up config

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 export default {
   emailjs: {
-    REACT_APP_EMAILJS_RECEIVER: 'rakin@example.com',
+    REACT_APP_EMAILJS_RECEIVER: 'rakin@example.com',  // change this when developing
+    REACT_APP_EMAILJS_RECEIVER_NAME: 'Rakin Uddin', // this too
     REACT_APP_EMAILJS_TEMPLATEID: 'template_F6qDjOze',
     REACT_APP_EMAILJS_USERID: 'user_FHgGH0FpcHyTjeDoR52UB',
-    REACT_APP_EMAILJS_RECEIVER_NAME: 'Rakin Uddin',
     SERVICE_ID: 'sendgrid'
   }
 }
