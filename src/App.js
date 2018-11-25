f3ec1f2d00f86ab8743bb415ec419a07dd8a9539
rakinuddin <rakinuddin97@gmail.com>
2018-11-25 12:22:48 -0500

    converted some components to stateless ones

--- src/App.js
+++ src/App.js
@@ -1,17 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import NavInstance from "./components/NavInstance";
 import Support from "./components/Support";
 import './App.css';
 
-class App extends Component {
-  render = () => {
-    return (
-      <div className="App">
-        <NavInstance/>
-        <Support/>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <div className="App">
+    <NavInstance/>
+    <Support/>
+  </div>
+)
 
 export default App;
--- src/components/NavInstance.js
+++ src/components/NavInstance.js
@@ -1,35 +1,31 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Navbar, NavbarBrand, NavLink, Nav, NavItem } from 'reactstrap';
 
-class NavInstance extends Component {
-  render = () => {
-    return (
-      <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="https://www.joatspace.com/">
-            JoatSpace
-          </NavbarBrand>
-          <Nav className="ml-auto" navbar>
-            <NavItem>
-              <NavLink href="#about">
-                About
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#product">
-                Product
-              </NavLink>
-            </NavItem>
-            <NavItem>
-              <NavLink href="#support">
-                Support
-              </NavLink>
-            </NavItem>
-          </Nav>
-        </Navbar>
-      </div>
-    )
-  }
-}
+const NavInstance = () => (
+  <div>
+    <Navbar color="light" light expand="md">
+      <NavbarBrand href="https://www.joatspace.com/">
+        JoatSpace
+      </NavbarBrand>
+      <Nav className="ml-auto" navbar>
+        <NavItem>
+          <NavLink href="#about">
+            About
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#product">
+            Product
+          </NavLink>
+        </NavItem>
+        <NavItem>
+          <NavLink href="#support">
+            Support
+          </NavLink>
+        </NavItem>
+      </Nav>
+    </Navbar>
+  </div>
+)
 
 export default NavInstance;
